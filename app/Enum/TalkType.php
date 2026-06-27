f97a9301c0d09a6563f6c0aea5ae6c61cc7cf7d2
Drilon <drilon-saiti@hotmail.com>
2026-06-27 19:20:39 +0200

    add models,factories, and seeders

---
+++ app/Enum/TalkType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum TalkType: string
+{
+    case LIGHTNING = 'lightning';
+    case STANDARD = 'standard';
+    case KEYNOTE = 'keynote';
+
+
+}
---
+++ database/factories/TalkFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enum\TalkType;
+use App\Models\Talk;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Talk>
+ */
+class TalkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->sentence(),
+            'type' => fake()->randomElement(TalkType::cases())->value,
+            'length' => rand(15,60),
+            'abstract' => fake()->paragraph(),
+            'organizer_notes' => fake()->paragraph(),
+        ];
+    }
+}
